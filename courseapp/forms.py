aa6d0b20cb162e272e5a47797d0655a4067128e9
Jihyun-Choi <70438098+Jihyun-Choi@users.noreply.github.com>
2021-12-05 16:13:21 +0900

    modify form

--- courseapp/forms.py
+++ courseapp/forms.py
@@ -5,9 +5,6 @@ from courseapp.models import Course
 
 
 class CourseCreationForm(ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left',
-                                                           'style': 'height: auto;'}))
-
     class Meta:
         model = Course
-        fields = ['title', 'image', 'content']
+        fields = ['title', 'image', 'classnumber']
--- courseapp/models.py
+++ courseapp/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Course(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='course', null=True)
 
-    title = models.CharField(max_length=200, null=False)#제목이 꼭 있도록 null=False
-    image = models.ImageField(upload_to='course/', null=False)
+    title = models.CharField(max_length=200, null=False)#과목명
+    image = models.ImageField(upload_to='course/', null=False)#과목사진
 
-    content = models.TextField(null=False)#내용도 꼭 있도록 null=False
+    classnumber = models.CharField(max_length=200, null=False)#학수번호
 
     created_at = models.DateField(auto_now_add=True, null=True)
